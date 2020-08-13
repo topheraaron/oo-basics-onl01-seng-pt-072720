c07d5701bff78ce99489ebc336c8c39a26948edd
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-13 02:57:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 class Shoe
-
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   def initialize(brand)
     @brand = brand
   end
 
-  def brand
-    @brand
-  end
+  #def brand
+  #  @brand
+  #end
 end
