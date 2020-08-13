01b79b9adaccd4cf6fcdbf3f1e127d5da4517c62
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-13 02:47:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,5 @@
 class Book
+  attr_accessor :author, :page_count, :genre
 
   def initialize(title)
     @title = title
@@ -8,31 +9,7 @@ class Book
     @title
   end
 
-  def author=(author)
-    @author = author
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
   end
-
-  def author
-    @author
-  end
-
-  def page_count=(num)
-    @page_count = num
-  end
-
-  def page_count
-    @page_count
-  end
-
-    def genre=(genre)
-      @genre = genre
-    end
-
-    def genre
-      @genre
-    end
-
-    def turn_page
-      puts "Flipping the page...wow, you read fast!"
-    end
 end
