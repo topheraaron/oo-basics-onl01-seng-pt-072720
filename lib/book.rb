93907a7829270d19eb0dcf9434f61643348f8292
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-13 02:33:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -19,7 +19,16 @@ class Book
   def page_count=(num)
     @page_count = num
   end
+
   def page_count
     @page_count
   end
+
+    def genre=(genre)
+      @genre = genre
+    end
+
+    def genre
+      @genre
+    end
 end
